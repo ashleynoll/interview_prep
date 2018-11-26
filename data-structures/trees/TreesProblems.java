1e2c6a8c10777160f6c84ad56b7d90fa0f29d3cc
Ashley Noll <ashleynoll@gatech.edu>
2018-11-25 20:11:53 -0800

    Add problems, solutions, and tests to trees

--- data-structures/trees/TreesProblems.java
+++ data-structures/trees/TreesProblems.java
@@ -1,11 +1,89 @@
 public class TreesProblems {
+    public static class Node<T> {
+        public T data;
+        public Node<T>[] children;
+        public Node<T> right;
+        public Node<T> left;
+
+        public Node(T data, Node<T> left, Node<T> right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public Node (T data) {
+            this(data, null, null);
+        }
+    }
+
     /**
      * Problem:
      *
+     * Given a sorted (increasing order) array with unique integer elements,
+     * write an algorithm to create a binary search tree with minimal height.
      *
+     * Solution Explanation:
+     *
+     * We will be using binary search to create an optimally balanced BST.
+     * The middle element of the array must be the root as it has equal
+     * elements to either side. Additionally, the now halved array has middle
+     * points which will be the optimal sub-tree root for their side.
+     */
+    public <T> Node<T> minimalTree(T[] arr) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+
+        return minimalTree(arr, 0, arr.length - 1);
+    }
+
+    private <T> Node<T> minimalTree(T[] arr, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = (left + right) / 2;
+        Node<T> node = new Node<>(arr[mid]);
+        node.left = minimalTree(arr, left, mid - 1);
+        node.right = minimalTree(arr, mid + 1, right);
+
+        return node;
+    }
+
+    /**
+     * Problem:
+     *
+     * Implement a function to check if a binary tree is balanced. For the purposes of
+     * this question, a balanced tree is defined to be a tree such that the heights of
+     * the two subtrees of any node never differ by more than one.
      *
      * Solution Explanation:
      *
      *
      */
+    public <T> boolean checkBalanced(Node<T> root) {
+        if (root == null) {
+            return true;
+        }
+
+        int difference = checkHeights(root);
+
+        return difference != Integer.MIN_VALUE;
+    }
+
+    private <T> int checkHeights(Node<T> curr) {
+        if (curr == null) {
+            return -1;
+        }
+
+        int leftHeight = checkHeights(curr.left),
+                rightHeight = checkHeights(curr.right);
+
+        if (leftHeight == Integer.MIN_VALUE || rightHeight == Integer.MIN_VALUE
+                || Math.abs(leftHeight - rightHeight) > 1) {
+            return Integer.MIN_VALUE;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 }
--- data-structures/trees/TreesTests.java
+++ data-structures/trees/TreesTests.java
@@ -1,2 +1,78 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TreesTests {
+    private static final long TIMEOUT = 200;
+    private TreesProblems problems;
+    TreesProblems.Node<Integer> root;
+
+    @Before
+    public void setUp() {
+        problems = new TreesProblems();
+    }
+
+    private static <T extends Comparable<T>> void assertTreeEquals(TreesProblems.Node<T> expectedRoot, TreesProblems.Node<T> actualRoot) {
+
+        if (expectedRoot == null && actualRoot != null) {
+            fail("Actual tree has an unexpected node " + "(data = " + actualRoot.data + ")");
+        } else if (actualRoot == null && expectedRoot != null) {
+            fail("Actual tree is missing a node" + "(expected data = " + expectedRoot.data + ")");
+        } else if (expectedRoot != null) {
+            assertEquals("Tree node contained incorrect data", expectedRoot.data, actualRoot.data);
+
+            assertTreeEquals(expectedRoot.left, actualRoot.left);
+            assertTreeEquals(expectedRoot.right, actualRoot.right);
+        }
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testMinimalTreeOdd() {
+        Integer[] arr = { 1, 2, 3, 4, 5 };
+
+        root = new TreesProblems.Node<>(3, new TreesProblems.Node<>(1, null,
+                new TreesProblems.Node<>(2)), new TreesProblems.Node<>(4, null, new TreesProblems.Node<>(5)));
+
+        assertTreeEquals(root, problems.minimalTree(arr));
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testMinimalTreeEven() {
+        Integer[] arr = { 1, 2, 3, 4, 5, 6 };
+
+        root = new TreesProblems.Node<>(3, new TreesProblems.Node<>(1, null,
+                new TreesProblems.Node<>(2)), new TreesProblems.Node<>(5, new TreesProblems.Node<>(4), new TreesProblems.Node<>(6)));
+
+        assertTreeEquals(root, problems.minimalTree(arr));
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testCheckBalancedFalse() {
+        root = new TreesProblems.Node<>(1, null, new TreesProblems.Node<>(3,  new TreesProblems.Node<>(4), null));
+
+        assertFalse("Tree should not be balanced.", problems.checkBalanced(root));
+
+        root = new TreesProblems.Node<>(1, new TreesProblems.Node<>(2, new TreesProblems.Node<>(4), null),
+                new TreesProblems.Node<>(3, new TreesProblems.Node<>(6, new TreesProblems.Node<>(7,
+                        new TreesProblems.Node<>(8), null), null), new TreesProblems.Node<>(5)));
+
+        assertFalse("Tree should not be balanced.", problems.checkBalanced(root));
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testCheckBalancedTrue() {
+        root = new TreesProblems.Node<>(1);
+
+        assertTrue("One node tree should be balanced.", problems.checkBalanced(root));
+
+        root = new TreesProblems.Node<>(1, new TreesProblems.Node<>(2), new TreesProblems.Node<>(3));
+
+        assertTrue("Tree should be balanced.", problems.checkBalanced(root));
+
+        root = new TreesProblems.Node<>(1, new TreesProblems.Node<>(2, new TreesProblems.Node<>(4), null),
+                new TreesProblems.Node<>(3, null, new TreesProblems.Node<>(5)));
+
+        assertTrue("Tree should be balanced.", problems.checkBalanced(root));
+    }
 }
